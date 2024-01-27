6625df05decdb3a2889e477040ffea2db77f2017
mktodorov86 <mktodorov86@gmail.com>
2024-01-27 14:46:37 +0200

    last

---
+++ Javascript/calculateSquareArea.js
@@ -0,0 +1,5 @@
+function calculateSquareArea(side){ const area = side*side;return area}
+const side = 4;
+const area = calculateSquareArea(side);
+    console.log("The area of squad is:" + area + " sq.cm.")
+
